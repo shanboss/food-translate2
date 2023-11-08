f3e9e30fe3175ea8dacb228bce41e64fff6403c2
Ryan McCord <mccordryan00@gmail.com>
2023-11-08 14:34:44 -0600

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 "use client";
 
-import Image from "next/image";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default function Home() {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
 
-  signInWithPopup(auth, googleProvider);
   return (
     <div>
-      <button onClick={signInWithPopup}>Sign In with Google</button>
+      <button onClick={() => signInWithPopup(auth, googleProvider)}>
+        Sign In with Google
+      </button>
     </div>
   );
 }
