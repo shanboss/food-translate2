58398ab712acd27f6f1bb6bf064ecf9452b572c9
Ryan McCord <mccordryan00@gmail.com>
2023-11-08 14:43:13 -0600

    dashbaord route

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,22 @@
 "use client";
 
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
+  const router = useRouter();
+
+  const signInWithGoogle = async function () {
+    await signInWithPopup(auth, googleProvider).then(() => {
+      router.push("/dashboard");
+    });
+  };
 
   return (
     <div>
-      <button onClick={() => signInWithPopup(auth, googleProvider)}>
-        Sign In with Google
-      </button>
+      <button onClick={signInWithGoogle}>Sign In with Google</button>
     </div>
   );
 }
